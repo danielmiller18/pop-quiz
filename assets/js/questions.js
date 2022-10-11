fc2fc832c768eb077451b57fd07dfc6a879dfc8a
Daniel Miller <104872554+danielmiller18@users.noreply.github.com>
2022-10-11 17:02:21 -0500

    Edits to both JS files after debugging

---
+++ assets/js/questions.js
@@ -0,0 +1,47 @@
+var questions = [
+    {
+        question: "Commonly used data types do not include ______",
+        answers: [
+            { text: "Strings", correct: false },
+            { text: "Booleans", correct: false },
+            { text: "Alerts", correct: true },
+            { text: "Numbers", correct: false },
+        ]
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ______",
+        answers: [
+            { text: "Quotes", correct: false },
+            { text: "Curly brackets", correct: false },
+            { text: "Parantheses", correct: true },
+            { text: "Square brackets", correct: false },
+        ]
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ______",
+        answers: [
+            { text: "Number and Strings", correct: false },
+            { text: "Other Arrays", correct: false },
+            { text: "Booleans", correct: false },
+            { text: "All of the above", correct: true },
+        ]
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables",
+        answers: [
+            { text: "Commas", correct: false },
+            { text: "Curly Brackets", correct: false },
+            { text: "Quotes", correct: true },
+            { text: "Paranthesis", correct: false },
+        ]
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: [
+            { text: "JavaScript", correct: false },
+            { text: "Terminal / Bash", correct: false },
+            { text: "For Loops", correct: false },
+            { text: "Console.log()", correct: true },
+        ]
+    },
+]
